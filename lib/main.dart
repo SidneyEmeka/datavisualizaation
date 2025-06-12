d33cc1a35870c17f3dc54748e06b9d1e15a0fd8a
Sidswipe <sidswipexyz@gmail.com>
2025-06-12 05:04:53 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Chart Demo',
+      title: 'Chart',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
